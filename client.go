854f5fd6771f7f80e43b5d3706b3cafa651cfa3c
Colby Rabideau <me@colbyr.com>
2014-06-12 01:48:20 -0700

    done!

--- client.go
+++ client.go
@@ -3,29 +3,89 @@ package main
 import (
   "bufio"
   "fmt"
+  "math/rand"
   "net"
+  "net/url"
   "os"
   "strings"
-  "net/url"
+  "time"
 )
+
+func decode(encoded []byte) (decoded string) {
+  decoded, _ = url.QueryUnescape(string(encoded))
+  return
+}
+
+func log(text string) {
+  fmt.Printf("%s\n> ", text)
+}
 
 func watchConsole(input chan string) {
   console := bufio.NewReader(os.Stdin)
   for {
     text, _ := console.ReadString('\n')
-    input <- strings.TrimSpace(text)
-    fmt.Print("> ")
+    if (strings.TrimSpace(text) != "") {
+      input <- strings.TrimSpace(text)
+    } else {
+      fmt.Print("> ")
+    }
   }
+}
+
+func listenForMessages(address string, messages chan string) {
+  for {
+    receiver, _ := net.Listen("tcp", address)
+    for {
+      connection, err := receiver.Accept()
+      if err != nil {
+        fmt.Printf("Error: %s\n", err)
+        continue
+      }
+      var command, name, message []byte
+      fmt.Fscan(connection, &command, &name, &message)
+      switch string(command) {
+      case "message":
+        messages <- fmt.Sprintf("(%s) %s", string(name), decode(message))
+      }
+    }
+  }
+}
+
+func sendCommand(command, name, text string) {
+  message := fmt.Sprintf("%s %s %s ", command, name, url.QueryEscape(text))
+  connection, _ := net.Dial("tcp", ":9999")
+  fmt.Fprint(connection, message)
+  connection.Close()
+  fmt.Print("> ")
+}
+
+func registerAddress(name string) (address string) {
+  rand.Seed(int64(time.Now().Nanosecond()))
+  port := rand.Intn(99999)
+  address = fmt.Sprintf("127.0.0.1:%d", port)
+  sendCommand("register", name, address)
+  return
 }
 
 func main() {
   input := make(chan string)
+  messages := make(chan string)
+
   go watchConsole(input)
+
   fmt.Print("What's your name? ")
   name := <-input
+
+  address := registerAddress(name)
+  log("Connected to server!")
+  go listenForMessages(address, messages)
+
   for {
-    receiver, _ := net.Dial("tcp", ":9999")
-    message := fmt.Sprintf("%s: %s", name, <-input)
-    fmt.Fprintf(receiver, "%s\n", url.QueryEscape(message))
+    select {
+    case text := <-input:
+      sendCommand("send", name, text)
+    case text := <-messages:
+      log(text);
+    }
   }
 }
--- server.go
+++ server.go
@@ -4,19 +4,79 @@ import (
   "fmt"
   "net"
   "net/url"
+  "time"
 )
+
+var clients = make(map[string]string)
 
 func decode(encoded []byte) (decoded string) {
   decoded, _ = url.QueryUnescape(string(encoded))
   return
 }
 
+func distribute(sender, message string) {
+  for to, _ := range clients {
+    if to == sender {
+      continue
+    }
+    go forwardMessage("message", to, sender, message)
+  }
+}
+
+func forwardMessage(command, to, sender, text string) {
+  message := fmt.Sprintf("%s %s %s ", command, sender, url.QueryEscape(text))
+  connection, err := net.Dial("tcp", clients[to])
+  if err != nil {
+    delete(clients, to)
+    distribute(to, "DISCONNECTED")
+    return
+  }
+  fmt.Fprint(connection, message)
+  connection.Close()
+}
+
+func ping(name string) {
+  connection, err := net.Dial("tcp", clients[name])
+  if err != nil {
+    go distribute(name, "DISCONNECTED")
+    delete(clients, name)
+    return
+  }
+  connection.Close()
+}
+
+func startPinging() {
+  for {
+    time.Sleep(1000 * time.Millisecond)
+    for name, _ := range clients {
+      go forwardMessage("ping", name, "SERVER", "")
+    }
+  }
+}
+
+func handleCommand(command, name, message string) {
+  switch command {
+  case "register":
+    clients[name] = message
+    fmt.Printf("%s registered at %s\n", name, message)
+    distribute(name, "CONNECTED")
+  case "send":
+    fmt.Printf("%s: %s\n", name, message)
+    distribute(name, message)
+  }
+}
+
 func main() {
   receiver, _ := net.Listen("tcp", ":9999")
+  go startPinging()
   for {
-    connection, _ := receiver.Accept()
-    var message []byte
-    fmt.Fscan(connection, &message)
-    fmt.Println(decode(message))
+    connection, err := receiver.Accept()
+    if err != nil {
+      fmt.Printf("Error: %s\n", err)
+      continue
+    }
+    var command, name, message []byte
+    fmt.Fscan(connection, &command, &name, &message)
+    handleCommand(string(command), string(name), decode(message))
   }
 }
